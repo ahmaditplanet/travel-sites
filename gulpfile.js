a42c7623dab3a0fc24c74c41e3bb7c4653d35544
Ahmad <ahmad.itplanet@gmail.com>
2017-10-29 00:35:37 +0200

    Browser Sync

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ var postcss = require('gulp-postcss');
  var autoprefixer = require('autoprefixer');
  var cssvars = require('postcss-simple-vars');
  var nested = require('postcss-nested');
- var cssimport = require('postcss-import');
+ var cssImport = require('postcss-import');
+ var browserSync = require('browser-sync').create();
 
 gulp.task('default', function(){
     console.log('This default task');
@@ -12,23 +13,33 @@ gulp.task('default', function(){
 
 gulp.task('html', function()
 {
-    console.log('HTML task');
+    
 });
 
 gulp.task('styles', function()
 {
     return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssimport,nested,cssvars,autoprefixer]))
+    .pipe(postcss([cssImport,nested,cssvars,autoprefixer]))
     .pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function()
 {
+    browserSync.init({
+        server: {
+            baseDir: "app"
+        }
+    });
     watch('./app/index.html', function(){
-        gulp.start('html'); 
+        browserSync.reload();
     });
     watch('./app/assets/styles/**/*.css', function(){
-        gulp.start('styles');
+        gulp.start('cssInject');
 
     });
-})
+});
+
+gulp.task('cssInject', ['styles'], function(){
+    return gulp.src('./app/temp/styles/styles.css')
+        .pipe(browserSync.stream());
+});
